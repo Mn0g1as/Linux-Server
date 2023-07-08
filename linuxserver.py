1a0f51565e8da27c2c2796a0beb08ef5afeadd1e
GoNz0 | Mn0g1as <128939384+Mn0g1as@users.noreply.github.com>
2023-07-08 18:01:06 +0200

    Update linuxserver.py

--- linuxserver.py
+++ linuxserver.py
@@ -1,1 +1,19 @@
+import socket
+
+ip = "192.168.1.X"
+port = 4444
+
+def main():
+  sock = socket.socket(socket.AF_INET,
+                       socket.SOCK_STREAM)
+  adress = (ip, port)
+  sock.bind(address)
+  sock.listen(1)
+  print("put the port: " + str(port))
+  conn , ipvictim = sock.accept()
+  msg = "This is an Message :D"
+  conn.send(msg.endcode())
+  conn.close()
+
+main()
 
